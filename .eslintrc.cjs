e7634913c2a5fefef65013ffa153f0d15e862c0a
Jae Webb <jynnae.webb@gmail.com>
2023-11-29 21:06:43 -0500

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: ['@nuxtjs/eslint-config-typescript'],
+  rules: {
+    'comma-dangle': 'error',
+    'eol-last': ['error', 'always'],
+    'max-len': ['error', { code: 80, comments: 80 }],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['warn', 'always'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
+};
